ba464335468c7b892a3dd41eb0ffa62b73dc7f36
matlo <mat.lau@laposte.net>
2019-09-01 21:14:49 +0200

    Use gimxtime library

--- test/timer.c
+++ test/timer.c
@@ -3,13 +3,6 @@
  License: GPLv3
  */
 
-#ifdef WIN32
-#include <windows.h>
-#else
-#include <sys/time.h>
-#include <stddef.h>
-#endif
-
 static int timer_close(void * user __attribute__((unused))) {
   done = 1;
   return 1;
@@ -21,20 +14,3 @@ static int timer_read(void * user __attribute__((unused))) {
    */
   return 1;
 }
-
-#ifndef WIN32
-static inline unsigned long long int get_time() {
-
-  struct timeval now;
-  gettimeofday(&now, NULL);
-  return now.tv_sec * 1000000 + now.tv_usec;
-}
-#else
-static inline unsigned long long int get_time() {
-
-  FILETIME ftime;
-  GetSystemTimeAsFileTime(&ftime);
-  LARGE_INTEGER li = { .HighPart = ftime.dwHighDateTime, .LowPart = ftime.dwLowDateTime };
-  return li.QuadPart / 10;
-}
-#endif
