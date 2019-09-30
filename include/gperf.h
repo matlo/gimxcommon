b30fe816df91d7a651acff5305362ea5e1cfac33
matlo <mat.lau@laposte.net>
2019-09-30 13:39:08 +0200

    Improve performance instrumentation macros.

--- include/gperf.h
+++ include/gperf.h
@@ -13,49 +13,51 @@
 #define GPERF_STR(s) #s
 
 #define GPERF_INST(NAME) \
-    static gtime gperf_##NAME##_sum = 0; \
-    static unsigned long long gperf_##NAME##_count = 0; \
-    static gtime gperf_##NAME##_start = 0; \
-    static gtime gperf_##NAME##_end = 0; \
-    static gtime gperf_##NAME##_worst = 0; \
-    static gtime gperf_##NAME##_diff = 0;
+    struct { \
+        gtime sum; \
+        unsigned long long count; \
+        gtime start; \
+        gtime end; \
+        gtime worst; \
+        gtime diff; \
+    } gperf_##NAME = {}
 
 #define GPERF_START(NAME) \
     do { \
-        gperf_##NAME##_start = gtime_gettime(); \
+        gperf_##NAME.start = gtime_gettime(); \
     } while (0)
 
 #define GPERF_END(NAME) \
     do { \
-        if (gperf_##NAME##_start != 0) { \
-           gperf_##NAME##_end = gtime_gettime(); \
-           gperf_##NAME##_diff = gperf_##NAME##_end - gperf_##NAME##_start; \
-           if (gperf_##NAME##_diff > gperf_##NAME##_worst) { \
-               gperf_##NAME##_worst = gperf_##NAME##_diff; \
+        if (gperf_##NAME.start != 0) { \
+           gperf_##NAME.end = gtime_gettime(); \
+           gperf_##NAME.diff = gperf_##NAME.end - gperf_##NAME.start; \
+           if (gperf_##NAME.diff > gperf_##NAME.worst) { \
+               gperf_##NAME.worst = gperf_##NAME.diff; \
            } \
-           gperf_##NAME##_sum += gperf_##NAME##_diff; \
-           gperf_##NAME##_count += 1; \
+           gperf_##NAME.sum += gperf_##NAME.diff; \
+           gperf_##NAME.count += 1; \
         } \
     } while (0)
 
-#define GPERF_TICK(NAME,TIME) \
+#define GPERF_TICK(NAME, TIME) \
     do { \
-        gperf_##NAME##_end = TIME; \
-        if (gperf_##NAME##_start != 0) { \
-           gperf_##NAME##_diff = gperf_##NAME##_end - gperf_##NAME##_start; \
-           if (gperf_##NAME##_diff > gperf_##NAME##_worst) { \
-               gperf_##NAME##_worst = gperf_##NAME##_diff; \
+        gperf_##NAME.end = TIME; \
+        if (gperf_##NAME.start != 0) { \
+           gperf_##NAME.diff = gperf_##NAME.end - gperf_##NAME.start; \
+           if (gperf_##NAME.diff > gperf_##NAME.worst) { \
+               gperf_##NAME.worst = gperf_##NAME.diff; \
            } \
-           gperf_##NAME##_sum += gperf_##NAME##_diff; \
-           gperf_##NAME##_count += 1; \
+           gperf_##NAME.sum += gperf_##NAME.diff; \
+           gperf_##NAME.count += 1; \
         } \
-        gperf_##NAME##_start = gperf_##NAME##_end; \
+        gperf_##NAME.start = gperf_##NAME.end; \
     } while (0)
 
 #define GPERF_LOG(NAME) \
     do { \
-        if (GLOG_LEVEL(GLOG_NAME,DEBUG) && gperf_##NAME##_count) { \
-            printf(GPERF_XSTR(NAME)": count = %I64u, average = %I64u, worst = %I64u\n", gperf_##NAME##_count, gperf_##NAME##_sum / gperf_##NAME##_count, gperf_##NAME##_worst); \
+        if (gperf_##NAME.count) { \
+            printf(GPERF_XSTR(NAME)": count = %I64u, average = %I64u, worst = %I64u\n", gperf_##NAME.count, gperf_##NAME.sum / gperf_##NAME.count, gperf_##NAME.worst); \
         } \
     } while (0)
 
