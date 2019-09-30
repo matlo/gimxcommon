89dfb2f699fa667ee9985bc9e61d23082d40c306
matlo <mat.lau@laposte.net>
2019-09-30 23:47:34 +0200

    Add samples to performance instrumentation. Uses static allocation.

--- include/gperf.h
+++ include/gperf.h
@@ -12,15 +12,42 @@
 #define GPERF_XSTR(s) GPERF_STR(s)
 #define GPERF_STR(s) #s
 
-#define GPERF_INST(NAME) \
-    struct { \
-        gtime sum; \
-        unsigned long long count; \
-        gtime start; \
-        gtime end; \
-        gtime worst; \
-        gtime diff; \
-    } gperf_##NAME = {}
+#define GPERF_INST(NAME, SAMPLETYPE, MAXSAMPLES) \
+   struct { \
+      gtime sum; \
+      unsigned long long count; \
+      gtime start; \
+      gtime end; \
+      gtime worst; \
+      gtime diff; \
+      SAMPLETYPE samples[MAXSAMPLES]; \
+      unsigned int maxsamples; \
+      unsigned int last; \
+      int wrapped; \
+   } gperf_##NAME = { .maxsamples = MAXSAMPLES }
+
+#define GPERF_SAMPLE(NAME) \
+   gperf_##NAME.samples[gperf_##NAME.last]
+
+#define GPERF_SAMPLE_INC(NAME) \
+   do { \
+      gperf_##NAME.last++; \
+      if (gperf_##NAME.last == gperf_##NAME.maxsamples) { \
+         gperf_##NAME.last = 0; \
+         gperf_##NAME.wrapped = 1; \
+      } \
+   } while (0)
+
+#define GPERF_SAMPLE_PRINT(NAME, SAMPLEPRINT) \
+   do { \
+      unsigned int gperf##NAME_first = gperf_##NAME.wrapped ? gperf_##NAME.last : 0; \
+      unsigned int gperf##NAME_nb = gperf_##NAME.wrapped ? gperf_##NAME.maxsamples : gperf_##NAME.last; \
+      unsigned int gperf##NAME_index; \
+      for (gperf##NAME_index = gperf##NAME_first; gperf##NAME_index < gperf##NAME_first + gperf##NAME_nb; ++gperf##NAME_index) { \
+          unsigned int gperf##NAME_index_r = gperf##NAME_index % gperf_##NAME.maxsamples; \
+          SAMPLEPRINT(gperf_##NAME.samples[gperf##NAME_index_r]); \
+      } \
+   } while (0)
 
 #define GPERF_START(NAME) \
     do { \
